7ce3d44c6f397712a9c56c879760669835cc2830
Tara Smith <tarasmith@Taras-Air.lan>
2022-05-03 19:56:47 -0400

    appended header

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 let content = document.querySelector('.js-generated.content')
+
+let header = document.createElement('h1')      
+header.setAttribute ('class', 'dog-name')         
+header.append ('Rizzo')
+content.append(header)
+
